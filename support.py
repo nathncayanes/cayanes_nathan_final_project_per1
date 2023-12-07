ab9c00aab7981e21ad7bd66a8f1b0e0c9563a53f
nathncayanes <n.cayanes9@gmail.com>
2023-12-07 08:55:18 -0800

    updated support.py

--- support.py
+++ support.py
@@ -2,14 +2,15 @@
 
 # all walk does is return the list with all the contents in the folder, which in this case would be the animations folder
 from os import walk
-# import pygame as pg
+import pygame as pg
 
 def import_folder(path):
     surface_list = []
     for folder_name, sub_folder, img_files in walk(path):
-        print(img_files)
-    #     for image in img_files:
-    #         full_path = path + "/" + image
-    #         image_surf = pg.image.load(full_path).convert_alpha()
-    #         image_surf.append(image_surf)
+        for image in img_files:
+            # prints the full path of each image
+            full_path = path + "/" + image
+            # this is going to allow us to load the images into a list and later call them in a loop
+            image_surf = pg.image.load(full_path).convert_alpha()
+            image_surf.append(image_surf)
     return surface_list
