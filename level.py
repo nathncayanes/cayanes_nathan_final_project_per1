db0e907607e2034472247f0917b0f6608f2d75a3
nathncayanes <n.cayanes9@gmail.com>
2023-12-11 14:42:38 -0800

    create camera class group

--- level.py
+++ level.py
@@ -9,12 +9,21 @@ class Level:
         # gets the display surface from main.py and allows me to "draw" directly on the display
         self.display_surface = pg.display.get_surface()
         # sprite groups
-        self.all_sprites = pg.sprite.Group()
+        self.all_sprites = CameraGroup()
         self.setup()
     def setup(self):
         self.player = Player((640,360), self.all_sprites)
     def run(self, dt):
         self.display_surface.fill(BLACK)
         # allows us to draw directly onto the display surface which is why we pass self.display_surface from above
-        self.all_sprites.draw(self.display_surface)
+        # self.all_sprites.draw(self.display_surface)
+        self.all_sprites.custom_draw()
         self.all_sprites.update(dt)
+
+class CameraGroup(pg.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pg.display.get_surface()
+    def custom_draw(self):
+        for sprite in self.sprites():
+            self.display_surface.blit(sprite.image, sprite.rect)
