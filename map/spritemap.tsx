e78717e34c5bfdba58496ddfb9b63c97606b7024
nathncayanes <n.cayanes9@gmail.com>
2023-12-14 09:47:13 -0800

    creating tiles

---
+++ map/spritemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="spritemap" tilewidth="128" tileheight="128" tilecount="100" columns="10">
+ <image source="images/spritemap.png" width="1280" height="1280"/>
+</tileset>
